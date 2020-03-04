f998ea2a52076acd08a4be0a85d3b2e30ecf6408
Sebastien <sle118@hotmail.com>
2020-03-04 13:02:14 -0500

    retrofit to gcc8/CMake

---
+++ writeSequeezeEsp.bat
@@ -0,0 +1,5 @@
+echo off
+
+set port=%1
+set baud=%2
+Python esptool.py --port %port% --baud %baud%  write_flash --flash_mode dio --flash_freq 80m --flash_size detect  0x1000 bootloader/bootloader.bin 0x8000 partitions.bin 0xd000 ota_data_initial.bin 0x10000 recovery.bin 0x150000 squeezelite.bin
