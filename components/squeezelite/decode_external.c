026abab1976ea7fb267fbaf92c363c5e6f5a1362
philippe44 <philippe44@users.noreply.github.com>
2020-03-14 23:51:53 -0700

    fixed BT - release

--- components/squeezelite/decode_external.c
+++ components/squeezelite/decode_external.c
@@ -127,28 +127,28 @@ static bool bt_sink_cmd_handler(bt_sink_cmd_t cmd, va_list args)
 		output.frames_played = 0;
 		_buf_flush(outputbuf);
 		if (decode.state != DECODE_STOPPED) decode.state = DECODE_ERROR;
-		LOG_INFO("BT audio sink started");
+		LOG_INFO("BT sink started");
 		break;
 	case BT_SINK_AUDIO_STOPPED:	
 		if (output.external == DECODE_BT) {
 			if (output.state > OUTPUT_STOPPED) output.state = OUTPUT_STOPPED;
 			output.stop_time = gettime_ms();
-			LOG_INFO("BT audio sink stopped");
+			LOG_INFO("BT sink stopped");
 		}	
 		break;
 	case BT_SINK_PLAY:
 		output.state = OUTPUT_RUNNING;
-		LOG_INFO("BT sink playing");
+		LOG_INFO("BT playing");
 		break;
 	case BT_SINK_STOP:		
 		_buf_flush(outputbuf);
 		output.state = OUTPUT_STOPPED;
 		output.stop_time = gettime_ms();
-		LOG_INFO("BT sink stopped");
+		LOG_INFO("BT stopped");
 		break;
 	case BT_SINK_PAUSE:		
 		output.stop_time = gettime_ms();
-		LOG_INFO("BT sink paused, just silence");
+		LOG_INFO("BT paused, just silence");
 		break;
 	case BT_SINK_RATE:
 		output.next_sample_rate = output.current_sample_rate = va_arg(args, u32_t);
