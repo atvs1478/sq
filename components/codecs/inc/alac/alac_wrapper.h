8f0709d5b1bef03ef4e877c3674982ff746eabfe
Philippe G <philippe44@users.noreply.github.com>
2020-05-04 18:40:48 -0700

    clarify our licensing intention

--- components/codecs/inc/alac/alac_wrapper.h
+++ components/codecs/inc/alac/alac_wrapper.h
@@ -1,22 +1,14 @@
 /*****************************************************************************
  * alac_wrapper.h: ALAC coder wrapper
  *
- * Copyright (C) 2016 Philippe <philippe44@outlook.com>
+ /* 
+ * (c) Philippe G. 2019, philippe_44@outlook.com
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
- *****************************************************************************/
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ * 
+ */
+ 
 #ifndef __ALAC_WRAPPER_H_
 #define __ALAC_WRAPPER_H_
 
